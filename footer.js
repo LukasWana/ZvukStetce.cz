adbab8605574c309079696f8a4bcbddc7676e902
Lukas <qwanap@gmail.com>
2025-10-31 11:14:10 +0100

    design fix

--- footer.js
+++ footer.js
@@ -5,12 +5,8 @@
 
   function getFooterHtml() {
     if (isEnglish()) {
-      // EN footer with centered image at 360px height
       return (
         '<footer class="contact">\n' +
-        '  <div style="width:100%;text-align:left;margin:0 0 12px 12px;">\n' +
-        '    <img src="../img/petra.jpg" alt="Petra" style="display:block;margin:0;height:200px;width:auto;" />\n' +
-        '  </div>\n' +
         '  <a class="social email-icon" href="">\n' +
         '    <img src="../img/mail_white.svg" height="40" alt="Email" />\n' +
         '  </a>\n' +
@@ -25,9 +21,6 @@
     }
     return (
       '<footer class="contact">\n' +
-      '  <div style="width:100%;text-align:left;margin:0 0 12px 12px;">\n' +
-      '    <img src="img/petra.jpg" alt="Petra" style="display:block;margin:0;height:200px;width:auto;" />\n' +
-      '  </div>\n' +
       '  <a class="social email-icon" href="">\n' +
       '    <img src="img/mail_white.svg" height="40" alt="Email" />\n' +
       '  </a>\n' +
